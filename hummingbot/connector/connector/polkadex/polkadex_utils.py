d60c21abfcef33ccfaee0f09bd7623955e53b647
Hounsette <houssein.d@gmail.com>
2021-10-07 08:21:12 +0900

    Polkadex helper class creation

--- hummingbot/connector/connector/polkadex/polkadex_utils.py
+++ hummingbot/connector/connector/polkadex/polkadex_utils.py
@@ -1,5 +1,8 @@
 from hummingbot.client.config.config_var import ConfigVar
 from hummingbot.client.settings import required_exchanges
+from scalecodec.base import RuntimeConfigurationObject
+from scalecodec.type_registry import load_type_registry_file, load_type_registry_preset
+
 
 CENTRALIZED = False
 EXAMPLE_PAIR = "LUNA-UST"
@@ -20,3 +23,12 @@ KEYS = {
                   is_secure=True,
                   is_connect_key=True),
 }
+
+
+class Polkadexhelper:
+
+    def __init__(self):
+        runtime_config = RuntimeConfigurationObject(ss58_format=42)
+        runtime_config.update_type_registry(load_type_registry_preset("default"))
+        runtime_config.update_type_registry(load_type_registry_file("polkadex_types.json"))
+        self.runtimeconfig = runtime_config
